6dfe593e456bcac926037b000c9bbbfa94be6c46
Dhruv Ahuja <83733638+dhruv-ahuja@users.noreply.github.com>
2024-08-11 23:58:59 +0530

    feat: enable items search filter on items table
    - hide rows that do not contain search term
    - reset rows when search input is cleared
    - refactor existing search button implementation to also reset table state

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 // common script code
 
-function clearItemsSearchInput() {
+function resetItemSearchFilter() {
+    // reset items table state 
+    const tableBody = document.getElementsByTagName('tbody')[0]
+    const itemRows = tableBody.getElementsByTagName('tr')
+
+    for (i = 0; i < itemRows.length; i++) {
+        const itemRow = itemRows[i]
+        itemRow.style.display = 'table-row'
+    }
+
+    // clear item search input
     const searchInput = document.getElementById('items-search')
     const clearButton = document.getElementById("items-search-clear-btn")
 
@@ -25,8 +35,28 @@ function enableItemsSearchClearButton() {
     }
 }
 
+function filterItemsByName() {
+    const searchInput = document.getElementById('items-search')
+    const searchValue = searchInput.value.toLowerCase().trim()
+
+    const tableBody = document.getElementsByTagName('tbody')[0]
+    const itemRows = tableBody.getElementsByTagName('tr')
+
+    for (i = 0; i < itemRows.length; i++) {
+        const itemRow = itemRows[i]
+        const itemNameColumn = itemRow.getElementsByTagName('td')[0]
+        const itemName = itemNameColumn.getElementsByClassName('table-item-name')[0]?.innerHTML.toLowerCase() ?? ""
+
+        if (!itemName.includes(searchValue)) {
+            itemRow.style.display = 'none'
+        } else {
+            itemRow.style.display = 'table-row'
+        }
+    }
+}
+
 // changes table header colours on scroll to ensure legibility
-const changeScrollingHeaderColor = () => {
+function changeScrollingHeaderColor() {
     const tableContainer = document.getElementById('items-table-wrapper');
     const headerCells = document.querySelectorAll('th');
 

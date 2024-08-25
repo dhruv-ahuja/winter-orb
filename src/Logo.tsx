c89002e40b5d863705ec1246a20f1d6140466a25
Dhruv Ahuja <83733638+dhruv-ahuja@users.noreply.github.com>
2024-08-25 17:05:31 +0530

    feat: add Logo and copy assets

---
+++ src/Logo.tsx
@@ -0,0 +1,11 @@
+const Logo = () => {
+    return (
+
+        <header id="logo" className="logo">
+            <img src="./logo.png" className="logo-img" />
+            <span className="logo-colored-text">Winter</span>Orb
+        </header>
+    )
+}
+
+export default Logo 

0c8b71b40d877450f9ab83dee2e6b4c0987095b0
Dhruv <83733638+dhruv-ahuja@users.noreply.github.com>
2024-09-12 07:14:18 +0530

    Requisite Fixes and Minor Improvements (#4)
    * fix: visual inconsistencies 
    
    * chore: make minor visual adjustments
    
    * feat: implement debounce for user search input
    
    * fix: resolve pagination issues
    
    - enable pagination after debounce impl.
    - fix incorrect items count
    - disallow exceeding total page count with `Next` button clicks
    
    * chore: fit item icons
    
    * chore: cleanup unnecessary comments
    
    * fix: ensure apt space between item price value and icon
    
    * chore: highlight currently selected category in the sidebar

--- src/components/ItemsContainer/ItemsContainer.tsx
+++ src/components/ItemsContainer/ItemsContainer.tsx
@@ -1,21 +1,84 @@
-import { useEffect, useState } from "react";
-import "./itemsContainer.css";
+import { useEffect, useMemo, useState } from "react";
+import debounce from "lodash/debounce";
 import ItemsFilterContainer from "./ItemsFilter/ItemsFilter";
 import ItemsTable from "./ItemsTable/ItemsTable";
-import {
-  baseTableRow,
-  isBackendError,
-  prepareCategoryName as prepareInternalCategoryName,
-  paginationQuery,
-  sortQuery,
-  filterQuery,
-} from "../../config";
 import { useGetItemsData } from "../../api/routes/poe";
 import { Pagination } from "../../api/schemas/common";
 import { useLocation, useNavigate } from "react-router-dom";
 import { categoryLinkMapping } from "../../api/schemas/poe";
+import {
+  paginationQuery,
+  sortQuery,
+  filterQuery,
+  baseTableRow,
+  prepareCategoryName,
+  isBackendError,
+} from "../../config";
+import "./itemsContainer.css";
 
 const PER_PAGE = 100;
+
+type refetchDataButtonProps = {
+  onButtonClick: () => void;
+};
+
+const RefetchDataButton = ({ onButtonClick }: refetchDataButtonProps) => {
+  return (
+    <div className="refetch-data-wrapper" onClick={() => onButtonClick()}>
+      An error occured, try getting data again?
+      <img src="/table/reload.svg" className="refetch-data-icon" />
+    </div>
+  );
+};
+
+type paginationElementProps = {
+  pagination: Pagination;
+  pageNumber: number;
+  onButtonClick: (pageNumber: number, totalPages: number) => void;
+  disablePageButtons: boolean;
+};
+const PaginationElement = ({ pagination, pageNumber, onButtonClick, disablePageButtons }: paginationElementProps) => {
+  const currentPage = pagination.page;
+  const itemsPerPage = pagination.per_page;
+  const totalItems = pagination.total_items;
+  const totalPages = pagination.total_pages;
+
+  let itemsStart = 1;
+  let itemsEnd = currentPage * itemsPerPage;
+
+  if (currentPage > 1) {
+    itemsStart = (currentPage - 1) * itemsPerPage + 1;
+  }
+
+  if (itemsEnd > totalItems) {
+    itemsEnd = totalItems;
+  }
+
+  return (
+    <div className="pagination">
+      <span>
+        Showing items {itemsStart} to {itemsEnd} of {totalItems}
+      </span>
+
+      <div className="paginationButtonGroup">
+        <button
+          className="paginationButton"
+          onClick={() => (pageNumber > 1 ? onButtonClick(pageNumber - 1, totalPages) : undefined)}
+          disabled={currentPage == 1 || disablePageButtons}
+        >
+          Prev
+        </button>
+        <button
+          className="paginationButton"
+          onClick={() => (itemsEnd < totalItems ? onButtonClick(pageNumber + 1, totalPages) : undefined)}
+          disabled={currentPage == totalPages || itemsEnd == totalItems || disablePageButtons}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 function filterTableData(searchInput: string, selectedItemType: string, itemRows: baseTableRow[]) {
   if (!searchInput && !selectedItemType) {
@@ -38,67 +101,11 @@ function filterTableData(searchInput: string, selectedItemType: string, itemRows
   return itemRows;
 }
 
-type refetchDataButtonProps = {
-  onButtonClick: () => void;
-};
-
-// TODO: replace table content with this button on failures
-const RefetchDataButton = ({ onButtonClick }: refetchDataButtonProps) => {
-  return (
-    <div className="refetch-data-wrapper" onClick={() => onButtonClick()}>
-      An error occured, try getting data again?
-      <img src="/table/reload.svg" className="refetch-data-icon" />
-    </div>
-  );
-};
-
-type paginationElementProps = {
-  pagination: Pagination;
-  pageNumber: number;
-  onButtonClick: (pageNumber: number) => void;
-  disablePageButtons: boolean;
-};
-const PaginationElement = ({ pagination, pageNumber, onButtonClick, disablePageButtons }: paginationElementProps) => {
-  const currentPage = pagination.page;
-  const itemsPerPage = pagination.per_page;
-
-  const itemsStart = currentPage == 1 ? currentPage : currentPage * itemsPerPage;
-  const itemsEnd = pagination.page == 1 ? currentPage * itemsPerPage : currentPage * itemsPerPage + itemsPerPage;
-
-  return (
-    <div className="pagination">
-      <span>
-        Showing items {itemsStart} to {itemsEnd} of {pagination?.total_items}
-      </span>
-
-      <div className="paginationButtonGroup">
-        <button
-          className="paginationButton"
-          onClick={() => onButtonClick(pageNumber - 1)}
-          disabled={currentPage == 1 || disablePageButtons}
-        >
-          Prev
-        </button>
-        <button
-          className="paginationButton"
-          onClick={() => onButtonClick(pageNumber + 1)}
-          disabled={currentPage == pagination.total_pages || disablePageButtons}
-        >
-          Next
-        </button>
-      </div>
-    </div>
-  );
-};
-
 const ItemsContainer = () => {
-  // TODO: perhaps move this to parent component
   const location = useLocation();
   const navigate = useNavigate();
-
   const category = categoryLinkMapping.get(location.pathname) ?? "Currency";
 
-  // TODO: handle this in a better way
   if (!category) {
     console.warn("redirecting");
     navigate("/");
@@ -106,36 +113,39 @@ const ItemsContainer = () => {
 
   const [searchInput, setSearchInput] = useState("");
   const [selectedItemType, setSelectedItemType] = useState("");
+  const [filteredRows, setFilteredRows] = useState<Array<baseTableRow>>([]);
   const [pageNumber, setPageNumber] = useState(1);
   const [disablePageButtons, setDisablePageButtons] = useState(false);
 
-  // TODO: fix search slow downs
   const paginationRequest: paginationQuery = {
     page: pageNumber,
     perPage: PER_PAGE,
   };
   const sortQueries: sortQuery[] = [{ field: "price_info.chaos_price", operation: "desc" }];
-  const filterQueries: filterQuery[] = [
-    { field: "category", operation: "=", value: prepareInternalCategoryName(category) },
-  ];
+  const filterQueries: filterQuery[] = [{ field: "category", operation: "=", value: prepareCategoryName(category) }];
 
-  const { data, isLoading, isError, error, refetch, isSuccess } = useGetItemsData({
+  const { data, isError, error, refetch, isSuccess, isFetched } = useGetItemsData({
     pagination: paginationRequest,
     sortQueries: sortQueries,
     filterQueries: filterQueries,
   });
 
+  function handlePaginationButtonClick(pageNumber: number, totalPages: number) {
+    if (pageNumber > 0 && pageNumber <= totalPages) {
+      setPageNumber(pageNumber);
+    }
+  }
+
+  const debouncedFilter = useMemo(() => {
+    return debounce((searchInput: string, selectedItemType: string, itemRows: baseTableRow[]) => {
+      const result = filterTableData(searchInput, selectedItemType, itemRows);
+      setFilteredRows(result);
+    }, 300);
+  }, []); // No dependencies here to create the debounce only once
+
   function refetchItemsData() {
     refetch();
-  }
-
-  function handlePaginationButtonClick(pageNumber: number) {
-    setPageNumber(pageNumber);
-  }
-
-  // TODO: show a skeleton or some transition state (if needed)
-  if (isLoading) {
-    console.log("loading data");
+    setDisablePageButtons(true);
   }
 
   if (isError) {
@@ -146,19 +156,30 @@ const ItemsContainer = () => {
     }
   }
 
-  const filteredRows = filterTableData(searchInput, selectedItemType, data?.itemRows ?? []);
-
-  // disable pagination buttons until we load the table with new data
   useEffect(() => {
-    refetch();
-    setDisablePageButtons(true);
-  }, [pageNumber, refetch]);
-
-  useEffect(() => {
-    if (isSuccess && disablePageButtons) {
+    if (isFetched && disablePageButtons) {
       setDisablePageButtons(false);
     }
-  }, [disablePageButtons, isSuccess]);
+  }, [disablePageButtons, isFetched]);
+
+  useEffect(() => {
+    if (isSuccess && data?.itemRows) {
+      if (!searchInput && !selectedItemType) {
+        setFilteredRows(data.itemRows);
+      } else {
+        debouncedFilter(searchInput, selectedItemType, filteredRows);
+      }
+    }
+
+    // Clean up debounce on unmount
+    return () => {
+      debouncedFilter.cancel();
+    };
+  }, [searchInput, selectedItemType, data?.itemRows, debouncedFilter, filteredRows, isSuccess]);
+
+  useEffect(() => {
+    refetch();
+  }, [paginationRequest.page, refetch]);
 
   return (
     <div id="items-container" className="items-container">

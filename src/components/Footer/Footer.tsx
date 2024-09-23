65591bfb0ddce49b9b35beeaff0b23c345c4ada8
Dhruv Ahuja <83733638+dhruv-ahuja@users.noreply.github.com>
2024-09-23 20:09:18 +0530

    feat: improve footer visuals
    - include footer row in grid rather than static space
    - adjust separately for mobile devices

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,16 +3,18 @@ import "./footer.css";
 const Footer = () => {
   return (
     <>
-      <hr className="footer-section" />
       <footer>
-        <div className="footer-declaration">
-          This product isn't affiliated with or endorsed by Grinding Gear Games in any way.
-        </div>
+        <hr className="footer-section" />
+        <div className="footer-content">
+          <div className="footer-declaration">
+            This product isn't affiliated with or endorsed by Grinding Gear Games in any way.
+          </div>
 
-        <div className="footer-icons">
-          <a href="https://github.com/dhruv-ahuja" target="_blank" rel="noopener">
-            <img className="footer-icon" src="/github.svg" />
-          </a>
+          <div className="footer-icons">
+            <a href="https://github.com/dhruv-ahuja" target="_blank" rel="noopener">
+              <img className="footer-icon" src="/github.svg" />
+            </a>
+          </div>
         </div>
       </footer>
     </>

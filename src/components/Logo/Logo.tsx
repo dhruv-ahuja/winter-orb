2f4b27439bf831b33395e2d5be2b692819997293
Dhruv Ahuja <83733638+dhruv-ahuja@users.noreply.github.com>
2024-09-23 08:01:08 +0530

    chore: add logo image alongside text

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -4,7 +4,9 @@ const Logo = () => {
   return (
     <header id="logo" className="logo">
       <img src="./logo.png" className="logo-img" />
-      <span className="logo-colored-text">Winter</span>Orb
+      <span className="logo-text">
+        <span className="logo-colored-text">Winter</span>Orb
+      </span>
     </header>
   );
 };
